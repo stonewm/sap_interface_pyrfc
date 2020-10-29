cbbb088a1db41623b969a3bd88eb9c076b3541e8
stonewm <stone.wangmin@qq.com>
2020-10-29 11:04:00 +0800

    pyrfc+flask restful service

--- pyrfc_flask_restful_service/SAP/GL.py
+++ pyrfc_flask_restful_service/SAP/GL.py
@@ -1,22 +1,74 @@
 from SAP import sap_system
 import pyrfc
 import tablib
+import json
 
-def get_ac_balances(cocd, glaccount, fiscal_year):
+
+def get_sap_connection():
     logon_params = sap_system.conn_params
     conn = pyrfc.Connection(**logon_params)
 
-    result = conn.call("BAPI_GL_ACC_GETPERIODBALANCES", 
-                        COMPANYCODE = cocd,  
-                        GLACCT = glaccount,
-                        FISCALYEAR = fiscal_year,
-                        CURRENCYTYPE = "10")   
-    
-    data = tablib.Dataset()
-    data.dict = result['ACCOUNT_BALANCES']
-    
-    return data.json
+    return conn
 
 
+class SAPGL(object):
+    def __init__(self) -> None:
+        self.sap_connection = get_sap_connection()
 
+    def get_ac_balances(self, cocd, g_laccount, fiscal_year):
+        """
+        获取会计科目在某一会计年度内按月份的发生额和余额
+        """
 
+        conn = self.sap_connection
+
+        result = conn.call("BAPI_GL_ACC_GETPERIODBALANCES",
+                           COMPANYCODE=cocd,
+                           GLACCT=g_laccount,
+                           FISCALYEAR=fiscal_year,
+                           CURRENCYTYPE="10")
+
+        data = tablib.Dataset()
+        data.dict = result['ACCOUNT_BALANCES']
+
+        return data.json
+
+    def get_all_acc_balances(self, cocd, fiscal_year):
+        """
+        获取所有会计科目在某一会计年度内按月份的发生额和余额
+        """
+
+        # result list for account balances
+        gl_acc_balances = []
+
+        accounts = self.get_gl_acc_list(cocd, '1')
+        data = tablib.Dataset()
+        data.json = accounts
+
+        gl_account_list = []
+        for item in data.dict:
+            gl_account_list.append(item.get('GL_ACCOUNT'))
+
+        for glacc in gl_account_list:
+            yearly_balances = json.loads(self.get_ac_balances(cocd, glacc, fiscal_year))
+
+            for period_balance in yearly_balances:
+                gl_acc_balances.append(period_balance)
+
+        return gl_acc_balances
+
+    def get_gl_acc_list(self, cocd, lang):
+        """
+        获取会计科目清单
+        """
+
+        conn = self.sap_connection
+
+        result = conn.call("BAPI_GL_ACC_GETLIST",
+                           COMPANYCODE=cocd,
+                           LANGUAGE=lang)
+
+        data = tablib.Dataset()
+        data.dict = result['ACCOUNT_LIST']
+
+        return data.json
--- pyrfc_flask_restful_service/main.py
+++ pyrfc_flask_restful_service/main.py
@@ -1,25 +1,51 @@
-from flask import Flask, request, make_response
-import SAP.GL
+from flask import Flask, request, make_response, jsonify
+from SAP.GL import SAPGL
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "index page"
 
 
-@app.route("/bal")
+@app.route("/accbal")
 def get_acc_balances():
     cocd = request.args.get('cocd')
     account = request.args.get('account')
     year = request.args.get('year')
 
-    ac_balances = SAP.GL.get_ac_balances(cocd, account, year)
+    sap_gl = SAPGL();
+    ac_balances = sap_gl.get_ac_balances(cocd, account, year)
 
     resp = make_response(ac_balances, 200)
     resp.headers['Content-Type'] = 'application/json'    
 
     return resp
+
+
+@app.route("/acclist")
+def get_gl_acc_list():
+    cocd = request.args.get('cocd')
+    lang = request.args.get('lang')
+
+    sap_gl = SAPGL();
+    gl_list = sap_gl.get_gl_acc_list(cocd, lang)
+
+    resp = make_response(gl_list, 200)
+    resp.headers['Content-Type'] = 'application/json'    
+
+    return resp
+
+
+@app.route('/balances')
+def get_all_acc_balances():
+    cocd = request.args.get('cocd')
+    fiscal_year = request.args.get('year')
+
+    sap_gl = SAPGL();
+    balances = sap_gl.get_all_acc_balances(cocd, fiscal_year)
+    return jsonify(balances)
 
 
 if __name__ == "__main__":
